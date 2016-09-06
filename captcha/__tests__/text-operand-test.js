95d6c8e26d20544eb02e26afc52a8c287ebc1e3d
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-09-06 15:33:16 +0700

    text-operand: implement other case

--- captcha/__tests__/text-operand-test.js
+++ captcha/__tests__/text-operand-test.js
@@ -1,13 +1,26 @@
 describe('TextOperand', function() {
   const TextOperand = require('../text-operand');
 
-  it('should be "One" when number is 1', function() {
-    let operand = new TextOperand(1);
-    expect(operand.toText()).toEqual('One');
+  [
+    { n: 1, expected: 'One' },
+    { n: 2, expected: 'Two' },
+    { n: 3, expected: 'Three' },
+    { n: 4, expected: 'Four' },
+    { n: 5, expected: 'Five' },
+    { n: 6, expected: 'Six' },
+    { n: 7, expected: 'Seven' },
+    { n: 8, expected: 'Eight' },
+    { n: 9, expected: 'Nine' },
+    { n: 0, expected: 'Zero' },
+  ].forEach(function(test) {
+    it('should be "' + test.expected + '" when number is ' + test.n, function() {
+      let operand = new TextOperand(test.n);
+      expect(operand.toText()).toEqual(test.expected);
+    });
   });
 
-  it('should be "Two" when number is 2', function() {
-    let operand = new TextOperand(2);
-    expect(operand.toText()).toEqual('Two');
+  it('should be empty string when number not match 1 digit', function() {
+    let operand = new TextOperand(10);
+    expect(operand.toText()).toEqual('');
   });
 });
--- captcha/text-operand.js
+++ captcha/text-operand.js
@@ -3,8 +3,19 @@ function TextOperand(n) {
 }
 
 TextOperand.prototype.toText = function() {
-  if (this.n === 2) return 'Two';
-  return 'One';
+  switch(this.n) {
+    case 1: return 'One';
+    case 2: return 'Two';
+    case 3: return 'Three';
+    case 4: return 'Four';
+    case 5: return 'Five';
+    case 6: return 'Six';
+    case 7: return 'Seven';
+    case 8: return 'Eight';
+    case 9: return 'Nine';
+    case 0: return 'Zero';
+    default: return '';
+  }
 };
 
 module.exports = TextOperand;
