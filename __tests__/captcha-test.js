1ada5a5d68df51c7bc9dd666f6c83520fe69c1bb
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-09-10 09:26:24 +0700

    add captcha

---
+++ __tests__/captcha-test.js
@@ -0,0 +1,22 @@
+describe('Captcha', function() {
+  it('should be One + 1 when mode=1, left operand=1, operator=1, right operand=1', function() {
+  });
+
+  it('should be Two + 1 when mode=1, left operand=2, operator=1, right operand=1', function() {
+  });
+
+  it('should be Three + 1 when mode=1, left operand=3, operator=1, right operand=1', function() {
+  });
+
+  it('should be One + 2 when mode=1, left operand=1, operator=1, right operand=2', function() {
+  });
+
+  it('should be 1 + One when mode=2, left operand=1, operator=1, right operand=1', function() {
+  });
+
+  it('should be 2 + One when mode=2, left operand=2, operator=1, right operand=1', function() {
+  });
+
+  it('should be 3 + One when mode=2, left operand=3, operator=1, right operand=1', function() {
+  });
+});
